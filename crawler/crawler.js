300995ad195fdc3ecda40ffea689b63db1c43a1b
xuyouyang <247082642@qq.com>
2014-10-28 20:01:11 +0800

    完成爬虫模块 功能：自动爬取每种类型12条记录存到数据库

--- crawler/crawler.js
+++ crawler/crawler.js
@@ -7,24 +7,38 @@ var Post = require('../models/post.js');
 
 exports.run = function () {
 	console.log('crawler run');	
-	getJob();
-	get
+	crawler('job');
+	crawler('news');
+	crawler('edu');
 };
 
-// 招聘信息模块
-var getJob = function () {
-	console.log()
-	var url = "http://www.cst.zju.edu.cn/index.php?c=Index&a=tlist&catid=6&p=1";
-	download (url, function (data) {
+var crawler = function (type) {
+	// 拼接url字符串
+	var url = 'http://www.cst.zju.edu.cn/index.php?c=Index&a=tlist&catid=';
+	switch (type) {
+		case 'job': 
+			url += 6;
+			break;
+		case 'edu':
+			url += 23;
+			break;
+		case 'news':
+			url += 72;
+			break;
+	}	
+	url += '&p=1';
+	// 请求网络，获取数据
+	download (url , function (data) {
+		// 如果连接成功
 		if (data) {
 			//console.log(data);
 			var $ = cheerio.load(data);
-		    // 招聘信息的网址
+			// 招聘信息的网址
 			var links = [];
-		    $('div > .lm_new > ul > li').each(function (i, e) {
+			$('div > .lm_new > ul > li').each(function (i, e) {
 			    //console.log("http://www.cst.zju.edu.cn/" + $(e).find("span.lm_new_zk > a").attr("href"));
 			    links[i] = "http://www.cst.zju.edu.cn/" + $(e).find("span.lm_new_zk > a").attr("href");
-		    });	    
+		    });	
 		    // 招聘信息的标题
 		    var titles = [];
 		    $('div > .lm_new > ul > li').each(function (i, e) {
@@ -37,64 +51,32 @@ var getJob = function () {
 			    //console.log($(e).find("span.fr").html());
 			    publish_times[i] = $(e).find("span.fr").html();
 		    });
-		    
+		    var posts = [];
 		    for (var i = 0; i < 12; i++) {
-		    	(function (i) {
-				    //console.log(links[i]);
-				    //console.log(titles[i]);
-				    //console.log(publish_times[i]);
-				    Post.getOne(titles[i], publish_times[i], function (err, post) {
-					    if (err) {
-						    console.log(err);
-					    } else {
-					    	console.log('已存在');
-					    	// 如果这条记录不存在，则添加
-					    	console.log(post);
-						    if (post == null) {
-							    var newPost = new Post({
-								    title : titles[i].toString(),
-								    link : links[i].toString(),
-									publish_time : publish_times[i].toString(),
-									type : "job",
-									top : 0
-							    });
-							    newPost.save( function(err, post) {
-								    if (err) {
-									    console.log(err);
-								    } else {
-								    	console.log('添加一条新通知');
-									    console.log(post);
-								    }
-							    });
-						    }
-					    }
-				    });
-				 })(i);
+			    var newPost = new Post({
+			    	title : titles[i].toString(),
+				    link : links[i].toString(),
+				    publish_time : publish_times[i].toString(),
+				    type : type,
+				    top : 0
+			    });
+			    posts[i] = newPost;
 		    }
+		    // 添加记录
+		    posts.forEach(function (newPost) {
+		    	//console.log(post);
+			    Post.getOne(newPost.title, newPost.publish_time, function (err, post) {
+				    if (!err && !post) {
+					    //console.log(post);
+					    newPost.save(function (err, post) {
+						    if (!err) {
+							    console.log('添加新记录');
+							    console.log(post);
+						    }
+					    });
+				    }
+			    });
+		    }); 
 		}
 	});
 };
-
-// 教务信息模块
-var getEdu = function () {
-	//var url = 
-};
-
-// 新闻模块
-var getNews = function () {
-	//var url = 
-};
-
-function download (url, callback) {
-	http.get(url, function (res) {
-		var data = "";
-		res.on('data', function (chunk) {
-			data += chunk;
-		});
-		res.on("end", function() {
-			callback(data);
-		});
-	}).on("error", function() {
-		callback(null);
-	});
-}
